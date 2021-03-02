293635f93d79c77fdee703e7417d0e7c1842984e
Adam Holecska <39831883+holecska@users.noreply.github.com>
2021-03-02 19:07:26 +0100

    database config var on heroku update

--- backend/database/config.py
+++ backend/database/config.py
@@ -6,4 +6,5 @@ class Config(object):
     DB_USERNAME = os.getenv('DB_USERNAME', 'postgres')
     DB_PASSWORD = os.getenv('DB_PASSWORD', 'admin')
     DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')
-    DB_PATH = "postgresql://{}:{}@{}/{}".format(DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME)
+    DB_PATH = os.getenv('DATABASE_URL') #"postgresql://{}:{}@{}/{}".format(DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME)
+    #DATABASE_URL = os.getenv('DATABASE_URL')
